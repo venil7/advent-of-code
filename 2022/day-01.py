e39525769e0f57a81ca261df7564efff3159bced
darkruby <art@darkruby.co.uk>
2023-04-18 08:41:21 +0100

    update day1/2022

--- 2022/day-01.py
+++ 2022/day-01.py
@@ -2,35 +2,31 @@ import os;
 
 file_name = "{dir}/data/day-01.txt".format(dir=os.path.dirname(__file__))
 
+def calorie_amounts():
+  with open(file_name, "rt") as file:
+    items = file.read().splitlines()
+  amount = 0;
+  for item in items:
+    if item.isdigit():
+      amount += int(item)
+    else:
+      yield amount
+      amount = 0
+  if amount > 0:
+    yield amount
+
 def max_number_per_group():
   max = 0
-  with open(file_name, "rt") as file:
-    items = file.read().splitlines()
-    current = 0
-    for item in items:
-      if item == "":
-        current = 0
-      else:
-        current += int(item)
-        if current > max:
-          max = current
-    return max
+  for amount in calorie_amounts():
+    if amount > max:
+      max = amount
+  
+  return max
 
 def top_three_per_group():
-  groups = []
-  with open(file_name, "rt") as file:
-    items = file.read().splitlines()
-    current = 0
-    for item in items:
-      if item == "":
-        groups.append(current)
-        current = 0
-      else:
-        current += int(item)
-    groups.append(current)
-
-    groups.sort(reverse=True)
-    return groups[0] + groups[1] + groups[2]
+  groups = list(calorie_amounts())
+  groups.sort(reverse=True)
+  return groups[0] + groups[1] + groups[2]
 
 print(max_number_per_group())
 print(top_three_per_group())
