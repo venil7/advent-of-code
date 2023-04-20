a0f8839d9e4b063e9d0bd8746c7ece4bb393d967
darkruby <art@darkruby.co.uk>
2023-04-20 21:53:50 +0100

    day-4 (1)

---
+++ 2022/day-04.py
@@ -0,0 +1,24 @@
+import os
+
+file_name = "{dir}/data/day-04.txt".format(dir=os.path.dirname(__file__))
+
+
+def items() -> list[str]:
+    with open(file_name, "rt") as file:
+        return [item
+                for item
+                in file.read().splitlines()]
+
+
+def expand_range(range_str: str) -> set[int]:
+    start, end = map(int, range_str.split('-'))
+    return set(range(start, end+1))
+
+
+def overlapping(items: list[str]) -> int:
+    ranges = [item.split(",") for item in items]
+    ranges = [tuple(map(expand_range, pair)) for pair in ranges]
+    return sum([1 for left, right in ranges if left.issubset(right) or right.issubset(left)])
+
+
+print(overlapping(items()))
