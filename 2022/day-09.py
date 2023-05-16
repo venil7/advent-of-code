95d94f2756c496f00501c39a24ccadbd9563da4f
darkruby <art@darkruby.co.uk>
2023-05-16 16:39:58 +0100

    day9/2

--- 2022/day-09.py
+++ 2022/day-09.py
@@ -2,8 +2,11 @@ import os
 
 file_name = "{dir}/data/day-09.txt".format(dir=os.path.dirname(__file__))
 
+Coord = tuple[int, int]
+Step = tuple[int, int]
 
-def parse_steps(s: str) -> list[tuple[int, int]]:
+
+def parse_steps(s: str) -> list[Step]:
     direction, steps = s.split(" ")
     steps = int(steps)
     if direction == "R":
@@ -16,7 +19,7 @@ def parse_steps(s: str) -> list[tuple[int, int]]:
         return [(0, -1) for _ in range(steps)]
 
 
-def steps() -> list[tuple[int, int]]:
+def steps() -> list[Step]:
     with open(file_name, "rt") as file:
         steps = [parse_steps(line) for line in file.read().splitlines()]
         return [step for sublist in steps for step in sublist]
@@ -26,31 +29,37 @@ def touching(p1: tuple[int, int], p2: tuple[int, int]) -> bool:
     return abs(p1[0] - p2[0]) <= 1 and abs(p1[1] - p2[1]) <= 1
 
 
-def keep_up(head:  tuple[int, int], tail: tuple[int, int]) -> tuple[int, int]:
-    if touching(head, tail):
-        return tail
+def keep_up(lead:  Coord, follow: Coord) -> tuple[int, int]:
+    if touching(lead, follow):
+        return follow
 
-    xdiff = head[0] - tail[0]
-    ydiff = head[1] - tail[1]
+    xdiff = lead[0] - follow[0]
+    ydiff = lead[1] - follow[1]
 
     # if in same column or longer distance by Y axis
     if xdiff == 0 or abs(xdiff) < abs(ydiff):
-        return (head[0], head[1] - 1 if ydiff > 0 else head[1] + 1)
+        return (lead[0], lead[1] - 1 if ydiff > 0 else lead[1] + 1)
 
     # if in same row or longer distance by X axis
     if ydiff == 0 or abs(xdiff) > abs(ydiff):
-        return (head[0] - 1 if xdiff > 0 else head[0] + 1, head[1])
+        return (lead[0] - 1 if xdiff > 0 else lead[0] + 1, lead[1])
+
+    return (lead[0] - 1 if xdiff > 0 else lead[0] + 1,
+            lead[1] - 1 if ydiff > 0 else lead[1] + 1)
 
 
-def calc_all_tail_movements(steps: list[tuple[int, int]]) -> int:
-    head, tail = (0, 0), (0, 0)
+def calc_tail_movements(steps: list[Step], length: int = 2) -> set[Coord]:
+    rope = [(0, 0) for _ in range(length)]
     tail_movements = set()
-    tail_movements.add(tail)
+    tail_movements.add(rope[-1])
+
     for (x, y) in steps:
-        head = (head[0] + x, head[1] + y)
-        tail = keep_up(head, tail)
-        tail_movements.add(tail)
-    return len(tail_movements)
+        rope[0] = (rope[0][0] + x, rope[0][1] + y)
+        for i in range(1, len(rope)):
+            rope[i] = keep_up(rope[i - 1], rope[i])
+        tail_movements.add(rope[-1])
+
+    return tail_movements
 
 
 def keep_up_test():
@@ -68,5 +77,17 @@ def keep_up_test():
     assert keep_up((4, 2), (3, 0)) == (4, 1)
 
 
+def part1(steps: list[Step] = steps()):
+    tail_movements = calc_tail_movements(steps)
+    print(len(tail_movements))
+
+
+def part2(steps: list[Step] = steps()):
+    tail_movements = calc_tail_movements(steps, 10)
+    print(len(tail_movements))
+
+
 keep_up_test()
-print(calc_all_tail_movements(steps()))
+
+part1()
+part2()
