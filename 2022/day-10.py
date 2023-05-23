f5a14a1bfb75903459751bc9cee2372a91c9ed4b
darkruby <art@darkruby.co.uk>
2023-05-23 14:49:14 +0100

    da10/2

--- 2022/day-10.py
+++ 2022/day-10.py
@@ -31,5 +31,25 @@ def task1():
     return sum(signals)
 
 
+def task2():
+    screen = [['.' for _ in range(40)] for _ in range(6)]
+    cycle, x = 0, 1
+    for command in commands():
+        for subcommand in command:
+            cycle += 1
+            x += subcommand
+
+            row, col = cycle//40, cycle % 40
+            if col == x or col == x-1 or col == x+1:
+                screen[row][col] = '#'
+    str = ''
+    for row in screen:
+        for pixel in row:
+            str += pixel
+        str += '\n'
+    return str
+
+
 if __name__ == "__main__":
     print(task1())
+    print(task2())
